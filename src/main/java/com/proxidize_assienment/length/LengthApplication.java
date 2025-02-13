29753f66acab0114286c019e9bc0ca8e33aff58d
Ismail Hamad <ismail.hamad@alawnehpay.com>
2025-02-13 22:31:56 +0300

    add spring inits

---
+++ src/main/java/com/proxidize_assienment/length/LengthApplication.java
@@ -0,0 +1,13 @@
+package com.proxidize_assienment.length;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LengthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LengthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proxidize_assienment/length/LengthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxidize_assienment.length;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LengthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
