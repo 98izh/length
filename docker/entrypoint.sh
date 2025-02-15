6e45e5801193e5fe2f5578326c55a444f84b9f64
Ismail Hamad <ismail.hamad@alawnehpay.com>
2025-02-15 21:37:03 +0300

    changing prometheus agent port

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,5 +4,5 @@ cd /home/proxidize/app/
 echo "Starting app length"
 FNAME=$(ls -1 /home/proxidize/app/length.jar)
 
-java -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9090:/home/proxidize/config.yaml -jar \
+java -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9091:/home/proxidize/config.yaml -jar \
 -Dspring.profiles.active=$ENVIRONMENT $JAVAOPS $FNAME
