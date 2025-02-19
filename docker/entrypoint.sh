b4357cecf64b5603e9aa6c1ad260211065af7088
Ismail Hamad <ismail.hamad@alawnehpay.com>
2025-02-19 21:23:21 +0300

    making necessary changes for OT

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 java --version
 cd /home/proxidize/app/
-echo "Starting app length"
-FNAME=$(ls -1 /home/proxidize/app/length.jar)
+echo "Starting app hash"
+FNAME=$(ls -1 /home/proxidize/app/hash.jar)
 
-java -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9091:/home/proxidize/config.yaml -jar \
--Dspring.profiles.active=$ENVIRONMENT $JAVAOPS $FNAME
+java $JAVAOPS \
+     -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9090:/home/proxidize/config.yaml \
+     -jar $FNAME
