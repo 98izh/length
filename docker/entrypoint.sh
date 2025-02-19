210974822d357f74842718054a8aa48e3fcdaa5b
Ismail Hamad <ismail.hamad@alawnehpay.com>
2025-02-19 21:37:35 +0300

    fixing typo

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 java --version
 cd /home/proxidize/app/
 echo "Starting app hash"
-FNAME=$(ls -1 /home/proxidize/app/hash.jar)
+FNAME=$(ls -1 /home/proxidize/app/length.jar)
 
 java $JAVAOPS \
      -javaagent:/home/proxidize/jmx_prometheus_javaagent.jar=9090:/home/proxidize/config.yaml \
